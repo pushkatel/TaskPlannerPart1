5907af726dd1518cfc144d338631b9f1cf450698
Kush Patel <kushpatel21@gmail.com>
2021-01-26 11:28:33 -0600

    marker added for chicago

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ mapboxgl.accessToken =
 
 const map = new mapboxgl.Map({
   container: 'map',
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center: [-87.641, 41.895], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
   style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
 });
+
+const markerCHI = document.createElement('div');
+markerCHI.style.width = '32px';
+markerCHI.style.height = '39px';
+markerCHI.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+new mapboxgl.Marker(markerCHI).setLngLat([-87.641, 41.895]).addTo(map);
